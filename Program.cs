e5913df7eff87fb1476d68912349b426cbcdd1c5
be2163@korea.ac.kr <be2163@korea.ac.kr>
2023-04-14 19:45:36 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ForGitHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "pineapple is yummy";
+            string findStr = "apple";
+            Console.WriteLine(input.IndexOf(findStr));
+        }
+    }
+}
