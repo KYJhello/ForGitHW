c5a9f47f02df5e283a3e29416c033c81dcb7b89f
be2163@korea.ac.kr <be2163@korea.ac.kr>
2023-04-14 20:16:56 +0900

    feat: Create Test1() that contains findStrIdx and make Test1() to Main()
    Test1() has the code that contains function for finding string index

--- Program.cs
+++ Program.cs
@@ -2,12 +2,18 @@
 {
     internal class Program
     {
-        // 문자열 속에 문자 찾기
-        static void Main(string[] args)
+        // 1. 문자열 속에 문자 찾기
+        static void Test1()
         {
             string input = "pineapple is yummy";
             string findStr = "apple";
             Console.WriteLine(input.IndexOf(findStr));
+        }
+        
+        static void Main(string[] args)
+        {
+            // 1. 문자열 속에 문자 찾기
+            Test1();
         }
     }
 }
