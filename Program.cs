44cef431a382ed184dc12b3716ed75ac2fbfd052
be2163@korea.ac.kr <be2163@korea.ac.kr>
2023-04-14 20:25:18 +0900

    docs: Comment of problem number 2
    This line has explanation of problem number 2's code

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@
             string findStr = "apple";
             Console.WriteLine(input.IndexOf(findStr));
         }
+
+        // 2. 문자열을 입력받으면 단어의 갯수를 출력하기
         
+
         static void Main(string[] args)
         {
             // 1. 문자열 속에 문자 찾기
